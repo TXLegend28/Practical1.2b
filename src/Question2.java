82237922a758a9fbd7c8bcc23864dca8acae55f9
TXLegend28 <garethzuma28@gmail.com>
2026-03-19 16:16:17 +0200

    Prac1.2b update2

--- src/Question2.java
+++ src/Question2.java
@@ -1,5 +1,16 @@
 public class Question2 {
     public static void main(String[] args) {
+        String name;
+        String email;
+        int contact = 102421;
+
+        name = "Gareth";
+        email = "garethzuma@gmail.com";
+
+        System.out.println(name);
+        System.out.println(email);
+        System.out.println(contact);
+
 
     }
 }

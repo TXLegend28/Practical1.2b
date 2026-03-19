4b08881f51ad8cffc2368a0ea62cafec502e84d0
TXLegend28 <garethzuma28@gmail.com>
2026-03-19 16:07:57 +0200

    Prac1.2b initial commit

--- src/Question1.java
+++ src/Question1.java
@@ -1,4 +1,8 @@
-package PACKAGE_NAME;
-
 public class Question1 {
+    public static void main(String[] args){
+        System.out.println("Computer Science");
+        System.out.println("Applied Mathematics");
+        System.out.println("Physical Science");
+        System.out.println("Nuclear Engineering");
+    }
 }
--- src/Question2.java
+++ src/Question2.java
@@ -1,4 +1,5 @@
-package PACKAGE_NAME;
-
 public class Question2 {
+    public static void main(String[] args) {
+
+    }
 }

31fadbfa304ca5f8293915cdd02dd78b916453c4
Gareth Zuma <garethzuma28@gmail.com>
2026-03-19 16:19:43 +0200

    Pract1.2b Que 3 Github update1

--- src/Question3.java
+++ src/Question3.java
@@ -1,5 +1,10 @@
 public class Question3 {
     public static void main(String[] args) {
-
+        
+        int totalInches = 86;
+        int feet = totalInches / 12;
+        int inches = totalInches % 12;
+        
+        System.out.println(totalInches + " inches = " + feet + " feet and " + inches + " inches");
     }
 }
